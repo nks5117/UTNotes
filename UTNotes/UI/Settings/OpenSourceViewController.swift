ef134d520249ecc7cef6bb0262354860b006bf30
nikesu <1026001096@qq.com>
2021-06-02 16:32:35 +0800

    opensource view enable zoom

--- UTNotes/UI/Settings/OpenSourceViewController.swift
+++ UTNotes/UI/Settings/OpenSourceViewController.swift
@@ -8,10 +8,10 @@
 import UIKit
 import SnapKit
 
-class OpenSourceViewController: UIViewController {
+class OpenSourceViewController: UIViewController, UIScrollViewDelegate {
     lazy var textView: UITextView = {
         let textView = UITextView(frame: .zero)
-        textView.font = .monospacedSystemFont(ofSize: 12, weight: .regular)
+        textView.font = .monospacedSystemFont(ofSize: 17, weight: .regular)
         if let url = Bundle.main.url(forResource: "opensource", withExtension: "") {
             textView.text = try? String(contentsOf: url)
         }
@@ -24,6 +24,9 @@ class OpenSourceViewController: UIViewController {
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView(frame: .zero)
         scrollView.backgroundColor = .systemBackground
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 0.1
+        scrollView.bouncesZoom = false
         return scrollView
     }()
     
@@ -31,9 +34,26 @@ class OpenSourceViewController: UIViewController {
         title = "Open Source Licenses"
         view.addSubview(scrollView)
         scrollView.addSubview(textView)
-        scrollView.contentSize = textView.contentSize
+        scrollView.contentSize = textView.frame.size
         scrollView.snp.makeConstraints { make in
             make.edges.equalTo(view)
         }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        scrollView.zoomScale = scrollView.bounds.width / scrollView.contentSize.width
+        scrollView.minimumZoomScale = scrollView.zoomScale
+    }
+}
+
+
+// UIScrollViewDelegate
+extension OpenSourceViewController {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        textView.subviews[1]
+    }
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        let size = textView.subviews[1].frame.size
+        scrollView.contentSize = CGSize(width: size.width * scale, height: size.height * scale)
     }
 }
